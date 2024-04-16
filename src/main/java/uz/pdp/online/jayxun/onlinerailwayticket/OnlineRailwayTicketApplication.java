e52f1c0aa108c51ea95982873d70a7e4098c2adb
Jayxunbey <muxammadovjayxun@gmail.com>
2024-04-16 12:27:05 +0500

    started

---
+++ src/main/java/uz/pdp/online/jayxun/onlinerailwayticket/OnlineRailwayTicketApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.jayxun.onlinerailwayticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRailwayTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRailwayTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/online/jayxun/onlinerailwayticket/OnlineRailwayTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.jayxun.onlinerailwayticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRailwayTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
