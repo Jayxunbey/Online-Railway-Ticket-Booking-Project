507fe4a41079718e46338553184503cb551effd5
Jayxunbey <muxammadovjayxun@gmail.com>
2024-04-17 09:39:18 +0500

    added security without JWT

---
+++ src/main/java/uz/pdp/online/jayxun/onlinerailwayticket/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package uz.pdp.online.jayxun.onlinerailwayticket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String[] WHITE_LIST =
+            {
+                    "/auth/**"
+            };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(config ->
+                        config.requestMatchers(WHITE_LIST).permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                ;
+
+        return http.build();
+    };
+
+
+}
